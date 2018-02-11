1fffb2e5a548c08c964fed5a94ca808a47276116
dannymcwaves <dannymcwaves96@gmail.com>
2018-02-11 19:11:49 +0000

    tests for the padmapper scrapper

--- scripts/pad_tests.js
+++ scripts/pad_tests.js
@@ -1,14 +1,14 @@
 
 let push = require('./padmapper');
 
-// push('Lappin Ave').then(data => {
-//     console.log(data)
-// }).catch(err => {
-//     console.log(err);
-// })
-
-push('Prescott Ave').then(data => {
+push('Dovercourt Rd').then(data => {
     console.log(data)
 }).catch(err => {
     console.log(err);
 })
+
+// push('Prescott Ave').then(data => {
+//     console.log(data)
+// }).catch(err => {
+//     console.log(err);
+// })
