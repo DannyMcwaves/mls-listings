5565c952717ccdde64ee0e9445fd4d6de8410453
dannymcwaves <dannymcwaves96@gmail.com>
2018-02-11 19:11:07 +0000

    tests for the whole scrape project

--- scrapy/index.js
+++ scrapy/index.js
@@ -5,6 +5,6 @@ let url = 'http://v3.torontomls.net/Live/Pages/Public/Link.aspx?Key=ded66b6ce5fb
 let url2 = 'http://v3.torontomls.net/Live/Pages/Public/Link.aspx?Key=dac85d3bfcea45608d96be99c34e95e4&App=TREB';
 let url3 = 'http://v3.torontomls.net/Live/Pages/Public/Link.aspx?Key=421f88db54b34781b2035e1be9679818&App=TREB';
 
-scrapy(url);
-// scrapy(url2);
+// scrapy(url);
+scrapy(url2);
 // scrapy(url3);
