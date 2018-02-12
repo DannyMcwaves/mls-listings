17a91ecc4cbeeb49b9b53590c7b6364edcbc8719
Albert Kim <albertkim91@gmail.com>
2018-02-12 15:56:57 -0500

    changing home db schema to not have spaces

--- db/home.js
+++ db/home.js
@@ -22,23 +22,23 @@ let schemaDef = {
     heat: String
   },
   income: {
-    '1 bedrooms': {
+    one_br: {
       averagePrice: String,
       numberOfUnits: Number
     },
-    '2 bedrooms': {
+    two_br: {
       averagePrice: String,
       numberOfUnits: Number
     },
-    '3 bedrooms': {
+    three_br: {
       averagePrice: String,
       numberOfUnits: Number
     },
-    '4 bedrooms': {
+    four_br: {
       averagePrice: String,
       numberOfUnits: Number
     },
-    'parking spot': String
+    parking_spot: String
   },
   totalIncome: String,
   totalExpenses: String,
