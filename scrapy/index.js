ced6ff740daa5dc36c5eab3145a0b194bfed436a
dannymcwaves <dannymcwaves96@gmail.com>
2018-02-26 00:34:42 +0000

    new test endpoints

--- scrapy/index.js
+++ scrapy/index.js
@@ -1,10 +1,7 @@
 
 let scrapy = require('./scrape');
 
-let url = 'http://v3.torontomls.net/Live/Pages/Public/Link.aspx?Key=ded66b6ce5fb4ed3af5881fc990c151e&App=TREB';
-let url2 = 'http://v3.torontomls.net/Live/Pages/Public/Link.aspx?Key=dac85d3bfcea45608d96be99c34e95e4&App=TREB';
-let url3 = 'http://v3.torontomls.net/Live/Pages/Public/Link.aspx?Key=421f88db54b34781b2035e1be9679818&App=TREB';
-
- scrapy(url);
-//scrapy(url2);
-// scrapy(url3);
+let url = 'http://v3.torontomls.net/Live/Pages/Public/Link.aspx?Key=da120e4cb97648dc985eed3a1259e965&App=TREB';
+let url2 = 'http://v3.torontomls.net/Live/Pages/Public/Link.aspx?Key=4ef972167b6345faaa441efd70fdc331&App=TREB';
+ 
+scrapy(url2);
